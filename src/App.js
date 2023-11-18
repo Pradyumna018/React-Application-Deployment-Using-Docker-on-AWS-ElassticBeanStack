c5bc9260f125dc696bd7589eca905d7526e1aae8
Pradyumna <pradyumna12@gmail.com>
2023-11-18 21:35:06 +0530

    React App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 style={{ fontSize: 80, color: 'green'  }}>Hello Pradyumna !</h1>
+      <h1 style={{ fontSize: 80, color: 'red'  }}>Hello Pradyumna !</h1>
     </div>
   );
 }
